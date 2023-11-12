fb2bcddf0f699e0428268092ac052e6166d68ac9
Tiago Villa <tiago.villa@edu.pucrs.br>
2023-11-12 16:59:54 -0300

    Adiciona funções auxiliares de geração de número aleatório e conversão de hexadecimal

---
+++ utils.ts
@@ -0,0 +1,23 @@
+import bigInt from "big-integer";
+
+/**
+ * Converte um valor hexadecimal para inteiro
+ * @param hex Valor em hexadecimal
+ * @returns
+ */
+export const hex2decimal = (hex: string) => {
+  return BigInt("0x" + hex).toString(10);
+};
+
+/**
+ * Recebe um valor N e gera um número aleatório com esse tamanho
+ * @param length Tamanho do número a ser gerado
+ * @returns
+ */
+export const getRandom = (length: number) => {
+  return bigInt(
+    Math.floor(
+      Math.pow(10, length - 1) + Math.random() * 9 * Math.pow(10, length - 1)
+    )
+  );
+};
